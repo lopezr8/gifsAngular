461478725e0459c4893bde3c38f0a9bc586ad6ff
Rafael Lopez <lopezrafael.b81@gmail.com>
2024-02-17 16:12:38 -0500

    commit final

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,10 @@ export class GifsService {
   private apiKey: string = 'kuKB9KjRoRc2PDo530fcTF1cYRr0lBrc';
   private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage();
+    console.log('Gifs Service Ready');
+   }
 
   get tagHistory(){
     return [...this._tagHistory];
@@ -24,8 +27,20 @@ export class GifsService {
     }
     this._tagHistory.unshift(tag);
     this._tagHistory = this.tagHistory.splice(0,10);
+    this.saveLocalStorage();
 
   }
+  private saveLocalStorage():void{
+    localStorage.setItem('history',JSON.stringify(this._tagHistory));
+  }
+  private loadLocalStorage():void{
+    if(!localStorage.getItem('history')) return;
+    this._tagHistory = JSON.parse(localStorage.getItem('history')! );
+    if(this._tagHistory.length === 0) return;
+
+    this.searchTag(this._tagHistory[0]);
+  }
+
 
   searchTag(tag:string):void{
     if(tag.length === 0) return;
